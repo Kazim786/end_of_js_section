1aa6f8c7087d5506713eb6154354b56f3da4db29
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-19 10:23:28 -0500

    made notes

--- codeWars.js
+++ codeWars.js
@@ -7,7 +7,9 @@ function descendingOrder(n){
   
     return parseInt(String(n).split('').sort().reverse().join(''))
     // String() function converts the value of an object to a string.
-    
+    //split method turns string into an array
+    //sort and reverse are array methods that sort in ascending order and then reverses it so its in descending order
+    //join turns array back into string
   }
   console.log(descendingOrder(234412));
 
