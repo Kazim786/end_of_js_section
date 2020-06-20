a389b57c3955790abdcd4ee1adda335fbbd70804
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-20 00:26:31 -0500

    number 2

--- codeWars.js
+++ codeWars.js
@@ -1,17 +1,57 @@
-//Codewars.com's exercise 
+// //Codewars.com's exercise 
 
-//make a function that sorts any given number in descending order
+// //make a function that sorts any given number in descending order
 
-function descendingOrder(n){
-    //...
+// function descendingOrder(n){
+//     //...
   
-    return parseInt(String(n).split('').sort().reverse().join(''))
-    // String() function converts the value of an object to a string.
-    //split method turns string into an array
-    //sort and reverse are array methods that sort in ascending order and then reverses it so its in descending order
-    //join turns array back into string
-  }
-  console.log(descendingOrder(234412));
+//     return parseInt(String(n).split('').sort().reverse().join(''))
+//     // String() function converts the value of an object to a string.
+//     //split method turns string into an array
+//     //sort and reverse are array methods that sort in ascending order and then reverses it so its in descending order
+//     //join turns array back into string
+//   }
+//   console.log(descendingOrder(234412));
 
-  //output: 443221
+//   //output: 443221
+
+//Exercise 2 - Find duplicate string
+
+//ignoreCase() => so it doesnt worry about the upper or lowercase
+function duplicateCount(str){
+    //...
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+      if(str[i] === str[i + 1]){
+      count = count + 1 //Count will tally how many times each given letter is repeated
+       return console.log(`${str[i]} occurs ${count}`)
+      } else {
+        return console.log('No return')
+      }
+      
+    }
+  }
+  
+  // duplicateCount('bbe'); //so far function tests code to see if it has a repeated number right after it.
+  duplicateCount('bba');
+  
+  
+  
+  // duplicateCount("AbCdeeF")
+  
+  // var example = 'hello';
+  
+  // var charRepeats = function(str) {
+  //     for (var i=0; i<str.length; i++) {
+  //       if ( str.indexOf(str[i]) !== str.lastIndexOf(str[i]) ) {
+  //         return false; // repeats
+  //       }
+  //     }
+  //   return true;
+  // }
+  
+  // let ex2 = 'bye'
+  // console.log(charRepeats(ex2))
+  
+  // console.log( charRepeats(example) ); // 'false', because when it hits 'l', the indexOf and lastIndexOf are not the same.
   
