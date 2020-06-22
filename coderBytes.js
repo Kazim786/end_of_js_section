2a985b3fe3991ddce74fbcfab315e9cc468e3c6a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-22 01:00:44 -0500

    Got a working code all by myself

--- coderBytes.js
+++ coderBytes.js
@@ -1,44 +1,34 @@
 // Number 1 Find Intersection
 
+
 const arr = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
 
-
-
-function FindIntersection(strArr, lookingFor) { 
-  let e1 = strArr[0].split();
+function FindIntersection(strArr) {
+  let e1 = strArr[0].split(", ");
+  
   strArr.unshift(e1)
+  strArr.splice(1, 1)
   
-  // strArr.splice(1, 1);
-  // strArr.pop()
-  // strArr.pop();
-
-strArr.splice(1, 1)
- let e2 = strArr[1].split();
-
- strArr.push(e2)
-
- strArr.splice(1, 1);
+  let e2 = strArr[1].split(", ");
   
+  strArr.push(e2)
+  strArr.splice(1, 1);
   console.log(strArr)
-
-  //so far ive made the strings into arrays. both arrays are within a parent array
-  let output = []
-  for(let i = 0; i < strArr.length; i++){
-    if(strArr[0][i] === strArr[1][i]){
-      output.push(strArr[0][i]);
-
+  console.log(strArr[1][3])
+  // So far I’ve made the strings into arrays. Both arrays are within a parent array.
+  
+  let output = [] // This will hold elements that intersect in both strings.
+  
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[0][i] === strArr[1][i]) {
+      console.log(output.push(strArr[0][i]));
     }
-    else{
-      console.log('no duplicates')
+    else {
+      console.log(`no duplicates ${i}`)
     }
   }
-
-  // code goes here  
-
-  //return lookingFor; 
+  
+  return output
 }
+
 console.log(FindIntersection(arr));
-   
-// keep this function call here 
-// console.log(FindIntersection(readline()));
-// console.log(FindIntersection(arr));
