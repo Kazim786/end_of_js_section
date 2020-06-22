203df4cd4d7ce0d4dfb3afd3f554d746828f6080
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-22 11:19:37 -0500

    exercise 3 codewars

--- codeWars.js
+++ codeWars.js
@@ -18,35 +18,35 @@
 //Exercise 2 - Find duplicate string
 
 //ignoreCase() => so it doesnt worry about the upper or lowercase
-function duplicateCount(str){
-    //...
-    let count = 0;
-    for(let i = 0; i < str.length; i++){
-      if(str[i] === str[i + 1]){
-      count = count + 1 //Count will tally how many times each given letter is repeated
-       return console.log(`${str[i]} occurs ${count}`)
-      } else {
-        return console.log('No return')
-      }
+// function duplicateCount(str){
+//     //...
+//     let count = 0;
+//     for(let i = 0; i < str.length; i++){
+//       if(str[i] === str[i + 1]){
+//       count = count + 1 //Count will tally how many times each given letter is repeated
+//        return console.log(`${str[i]} occurs ${count}`)
+//       } else {
+//         return console.log('No return')
+//       }
       
-    }
-  }
+//     }
+//   }
 
-//trying nested loops
-function duplicateCount(str) {
-    let result = {}
-    for(let i = 0; i < str.length; i++) {
-       const char = str[i]
-       if(char in result) {
-            result[char] +=1
-       } else {
-            result[char] = 1
-       }
-    }
-    return result
-  }
+// //trying nested loops
+// function duplicateCount(str) {
+//     let result = {}
+//     for(let i = 0; i < str.length; i++) {
+//        const char = str[i]
+//        if(char in result) {
+//             result[char] +=1
+//        } else {
+//             result[char] = 1
+//        }
+//     }
+//     return result
+//   }
   
-  console.table(duplicateCount('blaaaab'));
+//   console.table(duplicateCount('blaaaab'));
   
   // duplicateCount("AbCdeeF")
   
@@ -65,4 +65,14 @@ function duplicateCount(str) {
   // console.log(charRepeats(ex2))
   
   // console.log( charRepeats(example) ); // 'false', because when it hits 'l', the indexOf and lastIndexOf are not the same.
-  
+
+//Exercise 3
+
+// Usually when you buy something, 
+// you're asked whether your credit card number, 
+// phone number or answer to your most secret question is still 
+// correct. However, since someone could look over your shoulder, 
+// you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, 
+// which changes all but the last four characters into '#'.
