879147cb629667604cf1e4b2d5209971630f6cc8
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-24 15:27:30 -0500

    made new notes

--- coderBytes.js
+++ coderBytes.js
@@ -6,14 +6,14 @@ const arr = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"];
 function FindIntersection(strArr) {
   let e1 = strArr[0].split(", ");
   
-  strArr.unshift(e1)
-  strArr.splice(1, 1)
+  strArr.unshift(e1) // now the e1 array is on index 0
+  strArr.splice(1, 1) // the string which used to be on index 0 is on index 1 now and has to be removed
   
-  let e2 = strArr[1].split(", ");
+  let e2 = strArr[1].split(", "); // string 2 is back on index 1 and has to be turn into an array.
   
-  strArr.push(e2)
-  strArr.splice(1, 1);
-  console.log(strArr)
+  strArr.push(e2) //string 2 is an array now and is being put into the parent array
+  strArr.splice(1, 1); //string 2 is being removed
+  console.log(strArr) 
   console.log(strArr[1][3])
   // So far I’ve made the strings into arrays. Both arrays are within a parent array.
   
