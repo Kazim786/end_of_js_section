7dac1d26357b5532d48805745b0e30eeaeb95833
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-27 00:32:51 -0500

    did month exercise

--- edabit.js
+++ edabit.js
@@ -50,8 +50,77 @@ function countTrue(arr) {
 //********** */
 //Exercise 3 - how many words are there in a string:
 
-function countWords(str) {
-    const arrStr = str.split(" ")
+// function countWords(str) {
+//     const arrStr = str.split(" ")
     
-      return (arrStr.length)
+//       return (arrStr.length)
+//   }
+
+//************* */
+
+//Exercise 4:
+
+// Create a function that takes a number (from 1 to 12) 
+// and returns its corresponding month name as a string. For example, if you're given 3 as input, 
+// your function should return "March", 
+// because March is the 3rd month.
+
+function monthName(num) {
+    let month;
+  switch(num) {
+        case 1:
+            num === 1;
+            month = "January";
+            break;
+        case 2: 
+            num === 2;
+            month = "February";
+            break;
+        case 3: 
+            num === 3;
+            month = "March";
+            break;
+        case 4:
+            num === 4;
+            month = "April";
+            break;
+        case 5:
+            num === 5;
+            month = "May";
+            break;
+        case 6:
+            num === 6;
+            month = "June";
+            break;
+        case 7:
+            num === 7;
+            month = "July";
+            break;
+        case 8:
+            num == 8;
+            month = "August";
+            break;
+        case 9:
+            num === 9;
+            month = "September";
+            break;
+        case 10:
+            num === 10;
+            month = "October";
+            break;
+        case 11:
+            num === 11;
+            month = "November";
+            break;
+        case 12: 
+            num === 12;
+            month = "December";
+            break;
+        default: console.log("Num has to be between 1 or 12")
+        
+        }
+        return month
   }
+  
+  console.log(monthName(8));
+
