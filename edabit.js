635851cfc8c35633b38cc407622f2b91b2c0b26b
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-29 11:32:31 -0500

    modular

--- edabit.js
+++ edabit.js
@@ -211,6 +211,25 @@ function countTrue(arr) {
 //   console.log(countCharacters(theTwos))
 //It worked!!! WOOHOO DID IT ALL BY MYSELF WITH 0 HELP
 
+//************* */
+
+//Exercise 6
+
+// Create a function that goes through 
+// the array, incrementing (+1) for each 
+// odd number and decrementing (-1) for each even number.
+
+// Examples
+// transform([1, 2, 3, 4, 5]) ➞ [2, 1, 4, 3, 6]
+
+// transform([3, 3, 4, 3]) ➞ [4, 4, 3, 4]
+
+// transform([2, 2, 0, 8, 10]) ➞ [1, 1, -1, 7, 9]
+
+
+// Notes --> Will use modular
+
+
 
 
 
