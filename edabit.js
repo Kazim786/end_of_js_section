74f8ac7f267f67b583a5bf10459fe2d7080021c5
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-29 11:44:46 -0500

    time to test

--- edabit.js
+++ edabit.js
@@ -237,6 +237,20 @@ function countTrue(arr) {
     // array[i] -= 1
 //}
 
+const transform = (array) => {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            return array[i] += 1
+        }
+        else if(array[i] % 2 === 1 ){
+           return  array[i] -= 1
+        }
+      }
+      return console.log(array)
+  }
+
+
+
 
 
 
