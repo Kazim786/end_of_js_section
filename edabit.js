5bb3dc5eb5d27e908eb845563af6476f99d8b21b
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-06-30 01:37:26 -0500

    improved code

--- edabit.js
+++ edabit.js
@@ -287,7 +287,7 @@ function countTrue(arr) {
 
 //Code:
 const missingAngle = (a, b) => {
-    let missing = a + b - 180 
+    let missing = (a + b) - 180 
     if (missing === 90){
    return console.log( "Right angle")
   } else if (missing < 90){
