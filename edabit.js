b3d8db918202b801a63e1f9a4bfcc21e13d0dcb6
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-01 23:01:29 -0500

    made notes

--- edabit.js
+++ edabit.js
@@ -329,6 +329,22 @@ function countTrue(arr) {
 
 /* Fill in the regular expression */
 
+//************ */
+//Exercise 9
+
+// Create a function that takes an 
+// array and returns the difference between 
+// the biggest and smallest numbers.
+
+// Examples
+// diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+// // Smallest number is -50, biggest is 32.
+
+// diffMaxMin([44, 32, 86, 19]) ➞ 67
+// // Smallest number is 19, biggest is 86.
+// My Notes
+// Use min and max functions
+
 
 
 
