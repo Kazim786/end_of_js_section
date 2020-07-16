30904d746818a812c8cf2100285e7286a9644cd1
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-15 22:16:06 -0500

    exercise 2 of leetcode

--- leetcode.js
+++ leetcode.js
@@ -19,23 +19,46 @@
 
 
 //DONE :D
-var addTwoNumbers = function(l1, l2) {
-    //turn arrays into strings first and then join them then turn to numbers
-      l1 = l1.reverse()
-      l2 = l2.reverse()
-      l1 = l1.join("")
-      l2 = l2.join("")
-      l1 = Number(l1)
-      l2 = Number(l2)
+// var addTwoNumbers = function(l1, l2) {
+//     //turn arrays into strings first and then join them then turn to numbers
+//       l1 = l1.reverse()
+//       l2 = l2.reverse()
+//       l1 = l1.join("")
+//       l2 = l2.join("")
+//       l1 = Number(l1)
+//       l2 = Number(l2)
       
-      let sum = l1 + l2;
-      const sumArr = []
-      sum = sum.toString()
-      sum = sum.split("")
-      sumArr.push(sum)
+//       let sum = l1 + l2;
+//       const sumArr = []
+//       sum = sum.toString()
+//       sum = sum.split("")
+//       sumArr.push(sum)
       
     
-        console.log(sum)
-        return sumArr
-  };
+//         console.log(sum)
+//         return sumArr
+//   };
   
+//********************** */
+//EXERCISE 3
+
+// Longest Substring Without Repeating Characters
+//Given a string, find the length
+// of the longest substring without repeating characters.
+
+//Example 1:
+
+// Input: "abcabcbb"
+// Output: 3 
+// Explanation: The answer is "abc", with the length of 3. 
+// Example 2:
+
+// Input: "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3. 
+//              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
