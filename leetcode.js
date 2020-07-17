9ed347c29e1c4005d68c45dad75487bc7badba39
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-16 20:56:01 -0500

    did question one of async practice

--- leetcode.js
+++ leetcode.js
@@ -38,7 +38,7 @@
 //         console.log(sum)
 //         return sumArr
 //   };
-  
+
 //********************** */
 //EXERCISE 3
 
@@ -62,3 +62,7 @@
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3. 
 //              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+var lengthOfLongestSubstring = function(s) {
+    
+};
