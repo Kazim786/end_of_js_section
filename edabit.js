492adef25159eb7cd1ef087a987e02294d699dc4
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-03 23:12:56 -0500

    profit function

--- edabit.js
+++ edabit.js
@@ -622,7 +622,38 @@
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
 
-const toSeconds = (min) => {
-  let seconds = min * 60
-  return seconds
+// const toSeconds = (min) => {
+//   let seconds = min * 60
+//   return seconds
+// }
+
+//Exercise 16
+//Calculate the Profit
+
+// You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. You are given an object containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory. Return the total profit made, rounded to the nearest dollar. Assume all of the inventory has been sold.
+
+// Examples
+// profit({
+//   costPrice: 32.67,
+//   sellPrice: 45.00,
+//   inventory: 1200
+// }) ➞ 14796
+
+// profit({
+//   costPrice: 225.89,
+//   sellPrice: 550.00,
+//   inventory: 100
+// }) ➞ 32411
+
+// profit({
+//   costPrice: 2.77,
+//   sellPrice: 7.95,
+//   inventory: 8500
+// }) ➞ 44030
+// Notes
+// Profit = Total Sales - Total Cost
+
+function profit(info) {
+  let profit = info.sellPrice - info.costPrice
+  return profit
 }
